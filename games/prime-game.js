91c90cf649556d890b079ace228cfc03ee621207
maria <maaladina@yandex.ru>
2023-02-19 17:19:29 +0300

    add bin/brain-prime.js, games/prime-game.js

---
+++ games/prime-game.js
@@ -0,0 +1,36 @@
+import greet from '../src/cli.js';
+import askAndCheckQuestion from '../src/index.js';
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return 'no';
+  }
+  let divider = 2;
+  while (divider <= number / 2) {
+    if (number % divider === 0) {
+      return 'no';
+    }
+    divider += 1;
+  }
+  return 'yes';
+};
+
+const primeGame = () => {
+  const name = greet();
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  let score = 0;
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = Math.round(Math.random() * 100);
+    const question = `Question: ${randomNumber}`;
+    const rightAnswer = isPrime(randomNumber);
+    if (!askAndCheckQuestion(name, question, rightAnswer)) {
+      break;
+    }
+    score += 1;
+  }
+  if (score === 3) {
+    console.log(`Congratulations, ${name}`);
+  }
+};
+
+export default primeGame;
