79ae2e3a52eabbdc71f2f72a4ba9fc23144903d9
maria <maaladina@yandex.ru>
2023-03-02 15:41:28 +0300

    merged two function(index.js)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import readlineSync from 'readline-sync';
 
-const askAndCheckQuestion = (name, questionAndAnswer) => {
-  const [question, rightAnswer] = questionAndAnswer();
-  const userAnswer = readlineSync.question(`${question} `);
-  if (userAnswer === rightAnswer) {
-    console.log(`Your answer: '${userAnswer}'\nCorrect!`);
-    return true;
-  }
-  console.log(`Your answer:${userAnswer}.\n'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${name}!`);
-  return false;
-};
-
 const greetAndLoop = (task, questionAndAnswer) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
@@ -18,10 +7,15 @@ const greetAndLoop = (task, questionAndAnswer) => {
   console.log(task);
   let score = 0;
   for (let i = 0; i < 3; i += 1) {
-    if (!askAndCheckQuestion(name, questionAndAnswer)) {
+    const [question, rightAnswer] = questionAndAnswer();
+    const userAnswer = readlineSync.question(`${question} `);
+    if (userAnswer === rightAnswer) {
+      console.log(`Your answer: '${userAnswer}'\nCorrect!`);
+      score += 1;
+    } else {
+      console.log(`Your answer:${userAnswer}.\n'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${name}!`);
       break;
     }
-    score += 1;
   }
   if (score === 3) {
     console.log(`Congratulations, ${name}!`);
